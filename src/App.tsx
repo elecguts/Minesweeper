6a7f63db061dce9eed10aff810ff4df5099c3060
Samuel Gonzalez <Nomekop65@yahoo.com>
2024-08-06 20:43:35 -0400

    Basic skeleton code added to contact API and make a new game. Css is temporary functional until game works.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  const [game, setGame] = useState({
+    id: 1,
+    board: [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', 'x', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
+    state: 'new',
+    mines: 10,
+  })
+
+  async function handleNewGame() {
+    const body = { difficulty: 0 }
+    const response = await fetch(
+      'https://minesweeper-api.herokuapp.com/games',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(body),
+      }
+    )
+
+    if (response.ok) {
+      const newGame = await response.json()
+
+      setGame(newGame)
+    }
+  }
+
+  function handleClickCell(row: number, column: number) {
+    console.log(row, column)
+  }
+
+  return (
+    <div>
+      <h1>
+        <button onClick={handleNewGame}>New</button>
+      </h1>
+      <ul>
+        {game.board.map((boardRow, rowIndex) => {
+          return boardRow.map((cell, columnIndex) => {
+            return (
+              <li
+                key={columnIndex}
+                onClick={() => handleClickCell(rowIndex, columnIndex)}
+              >
+                {cell}
+              </li>
+            )
+          })
+        })}
+      </ul>
+    </div>
+  )
 }
